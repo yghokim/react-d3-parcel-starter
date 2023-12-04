d4f66b50b19a3783f8b481901fe816613c81f372
Young-Ho Kim <yghokim@younghokim.net>
2023-12-04 14:42:16 +0900

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import { App } from "./app";
+
+const rootDOM = document.getElementById("root")!;
+const root = createRoot(rootDOM);
+root.render(<App/>);
